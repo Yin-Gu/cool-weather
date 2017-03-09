2fdfb872f08967c2798cb656e8b750b4b73f2bd4
Yin <1065561759@qq.com>
2017-03-09 20:41:32 +0800

    完成遍历省市县三级列表的功能。

---
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -0,0 +1,46 @@
+package com.coolweather.app.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.coolweather.app.R;
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.CoolWeatherDB;
+import com.coolweather.app.model.County;
+import com.coolweather.app.model.Province;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ChooseAreaActivity extends Activity {
+	
+	public static final int LEVEL_PROVINCE = 0;
+	public static final int LEVEL_CITY = 1;
+	public static final int LEVEL_COUNTY = 2;
+	
+	private ProgressDialog progressDialog;
+	private TextView titleText;
+	private ListView listView;
+	private ArrayAdapter<String> adapter;
+	private CoolWeatherDB coolWeatherDB;
+	private List<String> dataList = new ArrayList<String>();
+	private List<Province> provinceList;
+	private List<City> cityList;
+	private List<County> countyList;
+	private Province selectedProvince;
+	private City selectedCity;
+	private int currentLevel;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.choose_area);
+	}
+	
+}
